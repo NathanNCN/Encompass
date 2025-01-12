ff9c186fd4f20e6cb86ea9f4e71bb9260b1b3409
NathanNCN <78819516+NathanNCN@users.noreply.github.com>
2025-01-12 03:31:56 -0500

    Update flashcard.js

--- static/flashcard.js
+++ static/flashcard.js
@@ -1,17 +1,17 @@
 
-// variables for tags in html
+// Variables for tags in html
 const add_card_button = document.getElementById('add-card');
 const remove_card_button = document.getElementById('remove-card');
 
 const div_container = document.getElementById('input-container');
 
-// variables
+// Variables
  let cardIndex = 5;
  let term = 'Term';
  let defin = 'Definition';
 
 function add_flash_card_inputs() {
-    //Create div container for input values
+    // Create div container for input values
     const newDiv = document.createElement("div");
     newDiv.className = "row mt-4 justify-content-around";
     newDiv.id = "div-input"
@@ -23,19 +23,19 @@ function add_flash_card_inputs() {
     newInputTerm.setAttribute("name", term.concat(cardIndex))
     newInputTerm.required = true;
 
-    // input for definition
+    // Input for definition
     const newInputDefinition = document.createElement("input");
     newInputDefinition.className = "col-5 form-control-lg text-center ml-5";
     newInputDefinition.placeholder = defin;
     newInputDefinition.setAttribute("name", defin.concat(cardIndex));
     newInputDefinition.required = true;
 
-    //insert the new inputs inside the container
+    // Insert the new inputs inside the container
     div_container.insertBefore(newDiv, document.getElementById('button-row'));
     newDiv.append(newInputTerm);
     newDiv.append(newInputDefinition);
 
-    //increase current index of items
+    // Increase the current index of items
     cardIndex++;
 
 }
@@ -45,16 +45,16 @@ function remove_card(){
 
 
 
-    // remove card if there are any cards
+    // Remove card if there are any cards
     if (cardIndex > 0){
-       // collect all div inputs
+       //Collect all div inputs
 
         const divs = document.querySelectorAll('#div-input');
 
-        // remove the last item from the divs
+        // Remove the last item from the divs
         divs[divs.length-1].remove();
 
-        // decrease index counter
+        // Decrease index counter
          cardIndex--;
     }
 
